750507925f175c253991a2f4132f544edec0d2c4
akphp <akramalsaidi9@gmail.com>
2020-10-13 23:52:25 +0300

    edit 002

--- resources/js/pages/auth/login.vue
+++ resources/js/pages/auth/login.vue
@@ -1,111 +1,100 @@
 <template>
-  <div class="row">
-    <div class="col-lg-8 m-auto">
 
-      <div class="q-pa-md">
-    <div class="q-gutter-md" >
-      <card :title="$t('login')">
-        <form @submit.prevent="login" @keydown="form.onKeydown($event)">
-          <!-- Email -->
-          <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
-            <div class="col-md-7">
+    <div class="q-pa-md q-mt-xl">
+        <div class="row justify-center">
+            <div class="col-12" style="max-width: 500px;">
+                <q-card>
+                    <q-card-section>
+                        <div class="text-h6">
+                            {{ $t('login') }}
+                        </div>
+                    </q-card-section>
 
-              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
+                    <q-separator/>
 
-              <has-error :form="form" field="email" />
+                    <form @submit.prevent="login" @keydown="form.onKeydown($event)">
+                        <div class="q-pa-lg">
+                            <div class="col-12 q-pb-lg q-mb-md">
+                                <div class="q-pl-xs q-pr-xs">
+                                    <q-input v-model="form.email" type="email" bottom-slots
+                                             :label="$t('email')" :error="form.errors.has('email')">
+                                        <template v-slot:error>
+                                            <has-error :form="form" field="email"/>
+                                        </template>
+                                    </q-input>
+                                </div>
+                                <div class="q-pl-xs q-pr-xs">
+                                    <q-input v-model="form.password" type="password" bottom-slots
+                                             :label="$t('password')" :error="form.errors.has('password')">
+                                        <template v-slot:error>
+                                            <has-error :form="form" field="password"/>
+                                        </template>
+                                    </q-input>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-6">
+                                        <q-checkbox v-model="remember" name="remember" :label="$t('remember_me')"/>
+                                    </div>
+                                    <div class="col-6 q-pt-sm">
+                                        <router-link :to="{ name: 'password.request' }" class="forgot-password">
+                                            {{ $t('forgot_password') }}
+                                        </router-link>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <q-btn type="submit" color="primary" :label="$t('login')" :loading="form.busy"/>
+                            </div>
+                        </div>
+
+                    </form>
+                </q-card>
             </div>
-          </div>
 
-          <!-- Password -->
-          <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('password') }}</label>
-            <div class="col-md-7">
-              <input v-model="form.password" :class="{ 'is-invalid': form.errors.has('password') }" class="form-control" type="password" name="password">
-              <has-error :form="form" field="password" />
-            </div>
-          </div>
-
-          <!-- Remember Me -->
-          <div class="form-group row">
-            <div class="col-md-3" />
-            <div class="col-md-7 d-flex">
-              <checkbox v-model="remember" name="remember">
-                {{ $t('remember_me') }}
-              </checkbox>
-
-              <!-- <router-link :to="{ name: 'password.request' }" class="small ml-auto my-auto">
-                {{ $t('forgot_password') }}
-              </router-link> -->
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <div class="col-md-7 offset-md-3 d-flex">
-              <!-- Submit Button -->
-              <v-button :loading="form.busy">
-                {{ $t('login') }}
-              </v-button>
-
-              <!-- GitHub Login Button -->
-              <login-with-github />
-            </div>
-          </div>
-        </form>
-      </card>
+        </div>
     </div>
-  </div>
 
-     </div>
-  </div>
 </template>
 
 <script>
 import Form from 'vform'
-import LoginWithGithub from '~/components/LoginWithGithub'
-
-import {
-  Quasar,
-  QInput
-} from 'quasar'
-
-
 export default {
-  middleware: 'guest',
-
-  components: {
-    LoginWithGithub , QInput
-  },
-
-  metaInfo () {
-    return { title: this.$t('login') }
-  },
-
-  data: () => ({
-    form: new Form({
-      email: '',
-      password: ''
+    middleware: 'guest',
+    layout: 'guest',
+    metaInfo () {
+        return { title: this.$t('login') }
+    },
+    data: () => ({
+        form: new Form({
+            email: '',
+            password: ''
+        }),
+        remember: false
     }),
-    remember: false
-  }),
-
-  methods: {
-    async login () {
-      // Submit the form.
-      const { data } = await this.form.post('/api/login')
-
-      // Save the token.
-      this.$store.dispatch('auth/saveToken', {
-        token: data.token,
-        remember: this.remember
-      })
-
-      // Fetch the user.
-      await this.$store.dispatch('auth/fetchUser')
-
-      // Redirect home.
-      this.$router.push({ name: 'home' })
+    methods: {
+        async login () {
+            // Submit the form.
+            const { data } = await this.form.post('/api/login')
+            // Save the token.
+            this.$store.dispatch('auth/saveToken', {
+                token: data.token,
+                remember: this.remember
+            })
+            // Fetch the user.
+            await this.$store.dispatch('auth/fetchUser')
+            // Redirect dashboard.
+            this.$router.push({ name: 'dashboard' })
+        }
     }
-  }
 }
 </script>
+
+<style scoped>
+    .forgot-password {
+        float: right;
+        color: #828c91;
+        text-decoration: none;
+    }
+</style>
--- resources/js/pages/auth/register.vue
+++ resources/js/pages/auth/register.vue
@@ -1,113 +1,98 @@
 <template>
-  <div class="row">
-    <div class="col-lg-8 m-auto">
-      <card v-if="mustVerifyEmail" :title="$t('register')">
-        <div class="alert alert-success" role="alert">
-          {{ $t('verify_email_address') }}
+
+    <div class="q-pa-md q-mt-lg">
+        <div class="row justify-center">
+            <div class="col-12" style="max-width: 500px;">
+                <q-card>
+                    <q-card-section>
+                        <div class="text-h6">
+                            {{ $t('register') }}
+                        </div>
+                    </q-card-section>
+
+                    <q-separator/>
+
+                    <form @submit.prevent="register" @keydown="form.onKeydown($event)">
+                        <div class="q-pa-lg">
+                            <div class="col-12 q-pb-lg q-mb-sm">
+                                <div class="q-pl-xs q-pr-xs">
+                                    <q-input v-model="form.name" type="text" bottom-slots
+                                             :label="$t('name')" :error="form.errors.has('name')">
+                                        <template v-slot:error>
+                                            <has-error :form="form" field="name"/>
+                                        </template>
+                                    </q-input>
+                                </div>
+                                <div class="q-pl-xs q-pr-xs">
+                                    <q-input v-model="form.email" type="email" bottom-slots
+                                             :label="$t('email')" :error="form.errors.has('email')">
+                                        <template v-slot:error>
+                                            <has-error :form="form" field="email"/>
+                                        </template>
+                                    </q-input>
+                                </div>
+                                <div class="q-pl-xs q-pr-xs">
+                                    <q-input v-model="form.password" type="password" bottom-slots
+                                             :label="$t('password')" :error="form.errors.has('password')">
+                                        <template v-slot:error>
+                                            <has-error :form="form" field="password"/>
+                                        </template>
+                                    </q-input>
+                                </div>
+                                <div class="q-pl-xs q-pr-xs">
+                                    <q-input v-model="form.password_confirmation" type="password" bottom-slots
+                                             :label="$t('confirm_password')"
+                                             :error="form.errors.has('password_confirmation')">
+                                        <template v-slot:error>
+                                            <has-error :form="form" field="password_confirmation"/>
+                                        </template>
+                                    </q-input>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <q-btn type="submit" color="primary" :label="$t('register')" :loading="form.busy"/>
+                            </div>
+                        </div>
+
+                    </form>
+                </q-card>
+            </div>
+
         </div>
-      </card>
-      <card v-else :title="$t('register')">
-        <form @submit.prevent="register" @keydown="form.onKeydown($event)">
-          <!-- Name -->
-          <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('name') }}</label>
-            <div class="col-md-7">
-              <input v-model="form.name" :class="{ 'is-invalid': form.errors.has('name') }" class="form-control" type="text" name="name">
-              <has-error :form="form" field="name" />
-            </div>
-          </div>
-
-          <!-- Email -->
-          <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
-            <div class="col-md-7">
-              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
-              <has-error :form="form" field="email" />
-            </div>
-          </div>
-
-          <!-- Password -->
-          <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('password') }}</label>
-            <div class="col-md-7">
-              <input v-model="form.password" :class="{ 'is-invalid': form.errors.has('password') }" class="form-control" type="password" name="password">
-              <has-error :form="form" field="password" />
-            </div>
-          </div>
-
-          <!-- Password Confirmation -->
-          <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('confirm_password') }}</label>
-            <div class="col-md-7">
-              <input v-model="form.password_confirmation" :class="{ 'is-invalid': form.errors.has('password_confirmation') }" class="form-control" type="password" name="password_confirmation">
-              <has-error :form="form" field="password_confirmation" />
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <div class="col-md-7 offset-md-3 d-flex">
-              <!-- Submit Button -->
-              <v-button :loading="form.busy">
-                {{ $t('register') }}
-              </v-button>
-
-              <!-- GitHub Register Button -->
-              <login-with-github />
-            </div>
-          </div>
-        </form>
-      </card>
     </div>
-  </div>
+
 </template>
 
 <script>
 import Form from 'vform'
-import LoginWithGithub from '~/components/LoginWithGithub'
-
 export default {
-  middleware: 'guest',
-
-  components: {
-    LoginWithGithub
-  },
-
-  metaInfo () {
-    return { title: this.$t('register') }
-  },
-
-  data: () => ({
-    form: new Form({
-      name: '',
-      email: '',
-      password: '',
-      password_confirmation: ''
+    middleware: 'guest',
+    layout: 'guest',
+    metaInfo () {
+        return { title: this.$t('register') }
+    },
+    data: () => ({
+        form: new Form({
+            name: '',
+            email: '',
+            password: '',
+            password_confirmation: ''
+        })
     }),
-    mustVerifyEmail: false
-  }),
-
-  methods: {
-    async register () {
-      // Register the user.
-      const { data } = await this.form.post('/api/register')
-
-      // Must verify email fist.
-      if (data.status) {
-        this.mustVerifyEmail = true
-      } else {
-        // Log in the user.
-        const { data: { token } } = await this.form.post('/api/login')
-
-        // Save the token.
-        this.$store.dispatch('auth/saveToken', { token })
-
-        // Update the user.
-        await this.$store.dispatch('auth/updateUser', { user: data })
-
-        // Redirect home.
-        this.$router.push({ name: 'home' })
-      }
+    methods: {
+        async register () {
+            // Register the user.
+            const { data } = await this.form.post('/api/register')
+            // Log in the user.
+            const { data: { token } } = await this.form.post('/api/login')
+            // Save the token.
+            this.$store.dispatch('auth/saveToken', { token })
+            // Update the user.
+            await this.$store.dispatch('auth/updateUser', { user: data })
+            // Redirect dashboard.
+            this.$router.push({ name: 'dashboard' })
+        }
     }
-  }
 }
 </script>
